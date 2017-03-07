8ac1e3e8354a4fc1ebd5994859af6ae6222dd154
ryanriebling <ryanriebling@gmail.com>
2017-03-06 20:20:27 -0800

    Initial Commit

---
+++ ImagesFramework/ImagesFramework.h
@@ -0,0 +1,19 @@
+//
+//  ImagesFramework.h
+//  ImagesFramework
+//
+//  Created by Ryan Riebling on 3/6/17.
+//  Copyright © 2017 Ryan Riebling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImagesFramework.
+FOUNDATION_EXPORT double ImagesFrameworkVersionNumber;
+
+//! Project version string for ImagesFramework.
+FOUNDATION_EXPORT const unsigned char ImagesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImagesFramework/PublicHeader.h>
+
+

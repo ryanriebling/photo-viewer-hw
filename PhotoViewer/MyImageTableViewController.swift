9100a982a07e6e1dba50084a2978967d92a80e54
ryanriebling <ryanriebling@gmail.com>
2017-03-06 21:55:45 -0800

    Added ImagesFramework integration for Homework 7 (week7)

--- PhotoViewer/MyImageTableViewController.swift
+++ PhotoViewer/MyImageTableViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import ImagesFramework
 
 class MyImageTableViewController: UIViewController {
 
-    static let myImages = ["masterChef", "master_chief_chibi", "mastercheif_chibi2","Halo_Master_Chief_Blue", "8bitChief", "chiefCartoon"]
+//    static let myImages = ["masterChef", "master_chief_chibi", "mastercheif_chibi2","Halo_Master_Chief_Blue", "8bitChief", "chiefCartoon"]
     static let myImageDescriptions = ["Master Chef loves to Cook!", "Master Chibi is determined", "Master Chibi is sad", "Master Chief is blue daba dee", "Cheif in 8bit" , "John 117 reporting for Duty"]
 
     @IBOutlet weak var myImageTableView: UITableView!
 
+    var imageManager: ImageManager!
+    var myImages: [String]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageManager = ImageManager()
+        myImages = imageManager.getAllImageNames()
+        print("Found images from manager \(myImages)")
         myImageTableView.delegate = self
         myImageTableView.dataSource = self
         myImageTableView.rowHeight = UITableViewAutomaticDimension
@@ -32,21 +39,33 @@ extension MyImageTableViewController : UITableViewDelegate, UITableViewDataSourc
 
     // MARK: - Table view data source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MyImageTableViewController.myImages.count
+        return myImages.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyImageCell", for: indexPath) as! MyImageTableViewCell
-        let myImageName = MyImageTableViewController.myImages[indexPath.row]
+        let myImageName = myImages[indexPath.row]
         let myImageDesc = MyImageTableViewController.myImageDescriptions[indexPath.row]
         cell.myImageTextView.text = myImageDesc
-        cell.setCustomImage(image: UIImage(named: myImageName)!)
+        var myImage: UIImage?
+        let myClosure = { (image: UIImage?) -> () in
+            print("Setting myImage \(myImage) to input image \(image)")
+            myImage = image!
+        }
+        imageManager.getImage(name: myImageName, imageClosure: myClosure)
+        cell.setCustomImage(image: myImage!)
         return cell
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let myImageName = MyImageTableViewController.myImages[indexPath.row]
-        let myImage = UIImage(named: myImageName)
+        let myImageName = myImages[indexPath.row]
+        var myImage: UIImage?
+        let myClosure = { (image: UIImage?) -> () in
+            myImage = image!
+        }
+        imageManager.getImage(name: myImageName, imageClosure: myClosure)
         return myImage!.size.height
     }
+
+
 }
